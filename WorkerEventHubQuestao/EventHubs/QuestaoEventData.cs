8c3d71caa7c478d81426d3c7a8ecccc21ad687aa
Renato Groffe <renato.groffe@yahoo.com.br>
2024-05-11 10:07:47 -0300

    Commit inicial
    Commit inicial

--- WorkerEventHubQuestao/EventHubs/QuestaoEventData.cs
+++
@@ -1,9 +0,0 @@
-namespace WorkerEventHubQuestao.EventHubs;
-
-public class QuestaoEventData
-{
-    public string? IdVoto { get; set; }
-    public string? Horario { get; set; }
-    public string? Producer { get; set; }
-    public string? Interesse { get; set; }
-}
